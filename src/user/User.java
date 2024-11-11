abb4f2ec9bfd26fa5e18de144f3b74fea44e2e09
KTNguyen04 <championvi12@gmail.com>
2024-11-11 14:57:10 +0700

    modify some stuff

--- src/user/User.java
+++ src/user/User.java
@@ -1,6 +1,15 @@
 package user;
 
+import java.text.SimpleDateFormat;
+
 class User {
     private String username;
     private String fullname;
+    private String address;
+    private String email;
+    private String dob;
+    private String gender;
+
+    User() {
+    }
 }
