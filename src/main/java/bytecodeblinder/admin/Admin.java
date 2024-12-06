4b3192461562f7fafffb9d62487ad37541da1e41
KTNguyen04 <championvi12@gmail.com>
2024-12-06 19:30:12 +0700

    init admin

--- src/main/java/bytecodeblinder/admin/Admin.java
+++ src/main/java/bytecodeblinder/admin/Admin.java
@@ -1,5 +1,17 @@
 package bytecodeblinder.admin;
 
-class Admin {
+import java.util.ArrayList;
 
+import bytecodeblinder.user.User;
+
+class Admin {
+    private ArrayList<User> users;
+
+    void setUsers(ArrayList<User> users) {
+        this.users = users;
+    }
+
+    ArrayList<User> getUsers() {
+        return this.users;
+    }
 }
